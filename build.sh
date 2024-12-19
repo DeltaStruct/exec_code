9717b2f6ec1522d9311ccb7f9fd494ca74190f14
DeltaStruct <DeltaStruct47@gmail.com>
2024-12-19 16:10:07 +0900

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,5 @@
+sudo apt install -y g++-12
+pip3 install flask
+curl -L https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb -o "cloudflared.deb"
+sudo apt install "cloudflared.deb"
+rm "cloudflared.deb"

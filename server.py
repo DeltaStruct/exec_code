8e3c1380a4bfb9a906c032a27c0cbe7b94c014f6
DeltaStruct <DeltaStruct47@gmail.com>
2024-12-22 15:28:10 +0900

    Update server.py

--- server.py
+++ server.py
@@ -49,7 +49,7 @@ if __name__=="__main__":
   while subprocess.run("grep -o \"https://.*\\.trycloudflare\\.com\" tmpinput.txt",shell=True,stdout=subprocess.PIPE).stdout=="":
     pass
   with open("url.txt",mode='w') as f:
-    f.write(subprocess.run("grep -o \"https://.*\\.trycloudflare\\.com\" tmpinput.txt",stdout=subprocess.PIPE).stdout)
+    f.write(subprocess.run("grep -o \"https://.*\\.trycloudflare\\.com\" tmpinput.txt",shell=True,stdout=subprocess.PIPE).stdout)
   os.system("""
     git config user.name "GitHub Actions"
     git config user.email "DeltaStruct@users.noreply.github.com"

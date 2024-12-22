f330cbb1a847391e17be1e00b0192da0f4427ead
DeltaStruct <DeltaStruct47@gmail.com>
2024-12-22 13:17:09 +0900

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask,request,jsonify
 from hashlib import sha512
+import subprocess
 import os
 
 app = Flask("server.py")
@@ -16,13 +17,28 @@ def exec_code():
   code = req["code"]
   code_name = req["code_name"]
   file_name = req["file_name"]
+  compile_timeout = int(req["compile_timeout"])
+  exec_timeout = int(req["exec_timeout"])
   compile_code = req["compile_code"]
   exec_code = req["exec_code"]
   stdin = req["stdin"]
   dir = "exec"+str(os.getpid())
   os.system("rm -rf "+dir)
   os.system("mkdir "+dir)
-  
+  with open(dir+'/'+code_name,mode='w') as f:
+    f.write(code)
+  res = subprocess.run("cd "+dir+" ; "+compile_code,shell=True,encoding="UTF-8",stdout=subprocess.PIPE,stderr=subprocess.PIPE,timeout=compile_timeout)
+  if !os.path.isfile(dir+'/'+file_name):
+    return jsonify({ "res": "CE", "exit_code": -1, "stdout": res.stdout, "stderr": res.stderr })
+  exr = subprocess.run("cd "+dir+" ; "+exec_code,shell=True,input=stdin,encoding="UTF-8",stdout=subprocess.PIPE,stderr=subprocess.PIPE,timeout=exec_timeout)
+  stdout = exr.stdout
+  stderr = exr.stderr
+  status = "OK"
+  if exr.returncode!=0:
+    stdout = res.stdout+stdout
+    stderr = res.stderr+stderr
+    status = "RE"
+  return jsonify({ "res": status, "exit_code": exr.returncode, "stdout": stdout, "stderr": stderr })
 
 if __name__=="__main__":
   app.run()

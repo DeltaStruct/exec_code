b1a6c09960b96c7be41022f11b2b55a491cc1729
DeltaStruct <DeltaStruct47@gmail.com>
2024-12-22 20:01:47 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 sudo apt install -y g++-12
 pip3 install flask
+pip3 install flask_cors
 curl -L https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb -o "cloudflared.deb"
 sudo apt install "./cloudflared.deb"
 rm "cloudflared.deb"

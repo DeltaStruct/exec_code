ce2d8cd9ea04a27f1746d9a46a3b82f615cc3e40
DeltaStruct <DeltaStruct47@gmail.com>
2024-12-23 09:30:49 +0900

    Update server.py

--- server.py
+++ server.py
@@ -40,6 +40,8 @@ def exec_code():
   if not os.path.isfile(dir+'/'+file_name):
     os.system("rm -rf "+dir)
     return jsonify({ "res": "CE", "exit_code": -1, "stdout": res.stdout, "stderr": res.stderr })
+  start = ""
+  exr = ""
   try:
     start = datetime.datetime.now()
     exr = subprocess.run("cd "+dir+" ; "+exec_code,shell=True,input=stdin,encoding="UTF-8",stdout=subprocess.PIPE,stderr=subprocess.PIPE,timeout=exec_timeout)

3abfde17737d31bd11befcb4f4a89d6b60407832
DeltaStruct <DeltaStruct47@gmail.com>
2024-12-23 12:08:00 +0900

    Update server.py

--- server.py
+++ server.py
@@ -53,7 +53,11 @@ def exec_code():
   status = "OK"
   os.system("rm -rf "+dir)
   tm = (end-start).days*86400*1000+(end-start).seconds*1000+(end-start).microseconds//1000
-  if exr==None or exr.returncode!=0:
+  if exr==None:
+    stdout = res.stdout+stdout
+    stderr = res.stderr+stderr
+    status = "TLE"
+  elif  exr.returncode!=0:
     stdout = res.stdout+stdout
     stderr = res.stderr+stderr
     status = "RE"

7fb96708a2172a5d362b75cbd4c65ab19c375a45
DeltaStruct <DeltaStruct47@gmail.com>
2025-01-04 19:48:01 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@ pip3 install flask_cors
 curl -L https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb -o "cloudflared.deb"
 sudo apt install "./cloudflared.deb"
 rm "cloudflared.deb"
+git clone https://github.com/atcoder/ac-library.git
+export CPLUS_INCLUDE_PATH="$CPLUS_INCLUDE_PATH:"pwd"/ac-library"

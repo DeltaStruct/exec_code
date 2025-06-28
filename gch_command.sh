b2e2efe940862228f89f385eaea572a64eb598d2
DeltaStruct <DeltaStruct47@gmail.com>
2025-06-28 20:18:12 +0900

    Update gch_command.sh

--- gch_command.sh
+++ gch_command.sh
@@ -2,3 +2,4 @@ cd /usr/include/x86_64-linux-gnu/c++/13/bits
 sudo mkdir stdc++.gch
 sudo g++ -Wall -Wextra -DLocal -std=gnu++20 stdc++.h -o stdc++.gch/h1.gch
 sudo g++ -Wall -Wextra -Wshadow -Wconversion -Wfloat-equal -fsanitize=address -fno-omit-frame-pointer -g -DLocal -D_GLIBCXX_DEBUG -std=gnu++20 stdc++.h -o stdc++.gch/h2.gch
+ls stdc++.gch

2630ecf91d5ac689d0b94d024971698f6556b77c
DeltaStruct <DeltaStruct47@gmail.com>
2025-12-21 00:09:54 +0900

    Update gch_command.sh for GCC 15.2.0

--- gch_command.sh
+++ gch_command.sh
@@ -1,5 +1,5 @@
-cd /usr/include/x86_64-linux-gnu/c++/13/bits
+cd /usr/include/x86_64-linux-gnu/c++/15/bits
 sudo mkdir stdc++.gch
-sudo g++ -Wall -Wextra -DLocal -std=gnu++20 stdc++.h -o stdc++.gch/h1.gch
-sudo g++ -Wall -Wextra -Wshadow -Wconversion -Wfloat-equal -fsanitize=address -fno-omit-frame-pointer -g -DLocal -D_GLIBCXX_DEBUG -std=gnu++20 stdc++.h -o stdc++.gch/h2.gch
+sudo g++-15.2.0 -Wall -Wextra -DLocal -std=gnu++23 stdc++.h -o stdc++.gch/h1.gch
+sudo g++-15.2.0 -Wall -Wextra -Wshadow -Wconversion -Wfloat-equal -fsanitize=address -fno-omit-frame-pointer -g -DLocal -D_GLIBCXX_DEBUG -std=gnu++23 stdc++.h -o stdc++.gch/h2.gch
 ls stdc++.gch

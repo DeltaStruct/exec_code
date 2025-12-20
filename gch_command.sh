8abc33bc78e8e3fa591d8c60e641b3c49ad68c61
DeltaStruct <DeltaStruct47@gmail.com>
2025-12-21 01:53:56 +0900

    Change directory for gch command script

--- gch_command.sh
+++ gch_command.sh
@@ -1,4 +1,4 @@
-cd /usr/include/x86_64-linux-gnu/c++/15/bits
+cd /usr/local/gcc-15.2.0/include/bits
 sudo mkdir stdc++.gch
 sudo g++-15.2.0 -Wall -Wextra -DLocal -std=gnu++23 stdc++.h -o stdc++.gch/h1.gch
 sudo g++-15.2.0 -Wall -Wextra -Wshadow -Wconversion -Wfloat-equal -fsanitize=address -fno-omit-frame-pointer -g -DLocal -D_GLIBCXX_DEBUG -std=gnu++23 stdc++.h -o stdc++.gch/h2.gch

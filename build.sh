2c4170c662c4f0c1d1a7bf3194f0dbc81f48adee
DeltaStruct <DeltaStruct47@gmail.com>
2025-12-20 19:42:58 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 sudo apt install -y g++-12
 g++-12 --version
-wget https://github.com/gcc-mirror/gcc/archive/refs/tags/releases/gcc-15.2.0.tar.gz
-tar xvf gcc-15.2.0.tar.gz
+wget https://github.com/gcc-mirror/gcc/archive/refs/tags/releases/gcc-15.2.0.tar.gz > /dev/null
+tar xvf gcc-15.2.0.tar.gz > /dev/null
 cd gcc-releases-gcc-15.2.0
 ./contrib/download_prerequisites
 ./configure --enable-languages=c,c++ --prefix=/usr/local/gcc-15.2.0 --disable-bootstrap --disable-multilib
@@ -10,7 +10,7 @@ sudo make install > /dev/null
 sudo ln -fs /usr/local/gcc-15.2.0/bin/g++ /usr/bin/g++-15.2.0
 sudo ln -fs /usr/local/gcc-15.2.0/bin/gcc /usr/bin/gcc-15.2.0
 g++ --version
-g++-15 --version
+g++-15.2.0 --version
 pip3 install flask
 pip3 install flask_cors
 curl -L https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb -o "cloudflared.deb"

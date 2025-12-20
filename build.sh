fb078112a66d99a0de56b3324fc8f2d3657c556b
DeltaStruct <DeltaStruct47@gmail.com>
2025-12-20 19:30:20 +0900

    Update G++-15

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
-sudo apt install -y g++-15
+sudo apt install -y g++-12
+g++-12 --version
+wget https://github.com/gcc-mirror/gcc/archive/refs/tags/releases/gcc-15.2.0.tar.gz
+tar xvf gcc-15.2.0.tar.gz
+cd gcc-releases-gcc-15.2.0
+./contrib/download_prerequisites
+./configure --enable-languages=c,c++ --prefix=/usr/local --disable-bootstrap --disable-multilib
+make
+g++ --version
 g++-15 --version
 pip3 install flask
 pip3 install flask_cors
